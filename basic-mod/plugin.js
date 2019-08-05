684ef3f70cdf9b1bff5043b7b5fda9c607f8e621
ac2pic <ac2pic@gmail.com>
2019-08-05 12:41:47 -0500

    Added plugin structure

---
+++ basic-mod/plugin.js
@@ -0,0 +1,18 @@
+export default class BasicMod extends Plugin {
+	constructor(mod) {
+		super(mod);
+		this.mod = mod;
+	}
+
+	async preload() {
+
+	}
+	
+	async postload() {
+
+	}
+
+	async prestart() {
+
+	}
+}
